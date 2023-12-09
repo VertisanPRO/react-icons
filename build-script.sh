deaf8e6b16ce6545b0fd60333628b2bfaee6ef0d
PadowYT2 <me@padow.ru>
2023-12-09 13:17:31 +0300

    Deploy

--- build-script.sh
+++ build-script.sh
@@ -6,4 +6,4 @@ time (cd packages/react-icons/ && yarn diff)
 (cd packages/_react-icons/ && npm pack |& tail)
 (cd packages/_react-icons-pack/ && npm pack |& tail)
 
-# (cd packages/preview/ && yarn build)
+(cd packages/preview/ && yarn build)
