126631adb4d45e54b9afca29c5f4a80bc0e66aa8
PadowYT2 <me@padow.ru>
2023-12-13 21:17:35 +0300

    Remove unused constants

--- packages/preview/src/utils/constants.ts
+++ packages/preview/src/utils/constants.ts
@@ -3,21 +3,3 @@ export const BRAND_TITLE_MONO = "react-icons";
 export const BRAND_DESCRIPTION =
   "Include popular icons in your React projects easily with react-icons.";
 export const BRAND_KEYWORDS = "React, Icons, Tree Shaking, SVG";
-
-export const HOME_USAGE = `
-import { FaBeer } from "@vertisanpro/react-icons/fa";
-
-class Question extends React.Component {
-  render() {
-    return <h3> Lets go for a <FaBeer />? </h3>
-  }
-}`;
-
-export const HOME_USAGE_PACK = `
-import { FaBeer } from "@vertisanpro/react-icons-pack/fa/FaBeer";
-
-class Question extends React.Component {
-  render() {
-    return <h3> Lets go for a <FaBeer />? </h3>
-  }
-}`;
