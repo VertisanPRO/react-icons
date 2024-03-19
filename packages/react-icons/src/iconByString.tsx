7dcc974ea5e357e22096dbc1473fac865cbe7aa1
PadowYT2 <me@padow.ru>
2024-03-19 20:59:27 +0300

    Fix importing..?

--- packages/react-icons/src/iconByString.tsx
+++ packages/react-icons/src/iconByString.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Folder } from "./Folder";
 
-export default async (folder: Folder, name: string): Promise<JSX.Element> => {
-  const module = await import(`./${folder}/index.esm.js`);
+export const IconByString = async (
+  folder: Folder,
+  name: string,
+): Promise<JSX.Element> => {
+  const module = await import(`./../../${folder}/index.esm.js`);
   const icon = module[name];
 
-  if (typeof icon === "function") return icon;
+  if (typeof icon === "function") return icon();
   return <></>;
 };
